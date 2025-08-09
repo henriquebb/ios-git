502c136b62ed500fff53ef46041e8cb121fb4512
Henrique Barbosa <henriqbarbosa@icloud.com>
2025-08-09 18:34:24 -0300

    Initial Commit

---
+++ iOS GitHub/iOS_GitHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_GitHubApp.swift
+//  iOS GitHub
+//
+//  Created by Henrique Batista Barbosa on 09/08/25.
+//
+
+import SwiftUI
+
+@main
+struct iOS_GitHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
